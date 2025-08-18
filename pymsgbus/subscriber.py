4a9f045e28ba62972b85836b8fe090a7aca8c698
lepton-flux <eric.m.cardozo@gmail.com>
2025-08-18 01:59:23 -0300

    Update LICENSE

--- pymsgbus/subscriber.py
+++ pymsgbus/subscriber.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Eric Cardozo
+# Copyright 2025 Eric Hermosis
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
